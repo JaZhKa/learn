3e50dd3843378f8e4bc23328d3d27d0826a983ce
Yakov Lemenkov <yakoff4@gmail.com>
2022-03-26 18:22:19 +0300

    фиксанул else но не до конца

--- U1/firstHomeWork.js
+++ U1/firstHomeWork.js
@@ -1,5 +1,5 @@
-let money = +prompt('Ваш бюджет на месяц?'),
-    time = prompt('Введите дату в формате YYYY-MM-DD');
+let money = +prompt('Ваш бюджет на месяц?', ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
     budget: money,
@@ -11,19 +11,17 @@ let appData = {
 };
 
 for (let i = 0; i < 2; i++) {
-    let exp = prompt('Введите обязательную статью расходов в этом месяце'),
-        expValue = +prompt('Во сколько это обойдется');
+    let exp = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        expValue = +prompt('Во сколько это обойдется', '');
 
         if ((typeof(exp)) === 'string' && (typeof(exp)) != null && (typeof(expValue)) != null && exp != '' && expValue != '' && exp.length < 50) {
             appData.expenses[exp] = expValue;
             appData.budget -= expValue;
-            appData.moneyDayBdget = appData.budget / 30;
-        } else if (appData.expenses.hasOwnProperty.NaN) {
+        } else {
             alert('Введите корректные значения');
         }
     }
 
-alert('Ваш дневной бюджет: ' + appData.moneyDayBdget);
+alert('Ваш дневной бюджет: ' + (appData.budget / 30));
 console.log(appData);
-console.log('Ваш дневной бюджет: ' + appData.moneyDayBdget);
-console.log(typeof(appData.moneyDayBdget))
+console.log('Ваш дневной бюджет: ' + (appData.budget / 30));
