ba9ff6e13a40cbcb98f66e9a1c8002b6a81d5864
Yakov Lemenkov <yakoff4@gmail.com>
2022-03-26 17:19:20 +0300

    не получилось добавить else if

--- U1/firstHomeWork.js
+++ U1/firstHomeWork.js
@@ -1,7 +1,7 @@
 let money = +prompt('Ваш бюджет на месяц?'),
     time = prompt('Введите дату в формате YYYY-MM-DD');
 
-const appData = {
+let appData = {
     budget: money,
     timeData: time,
     expenses: {},
@@ -14,14 +14,16 @@ for (let i = 0; i < 2; i++) {
     let exp = prompt('Введите обязательную статью расходов в этом месяце'),
         expValue = +prompt('Во сколько это обойдется');
 
-        // if ((typeof(exp)) === 'string' && (typeof(exp)) != null && (typeof(expValue)) != null && exp != '' && expValue != '' && exp.length < 50) {
-        //     appData.expenses -= expValue;
+        if ((typeof(exp)) === 'string' && (typeof(exp)) != null && (typeof(expValue)) != null && exp != '' && expValue != '' && exp.length < 50) {
             appData.expenses[exp] = expValue;
-        // }
-    
-}
-
-appData.moneyDayBdget = appData.budget / 30;
+            appData.budget -= expValue;
+            appData.moneyDayBdget = appData.budget / 30;
+        } else if (appData.expenses.hasOwnProperty.NaN) {
+            alert('Введите корректные значения');
+        }
+    }
 
 alert('Ваш дневной бюджет: ' + appData.moneyDayBdget);
-console.info(appData.expenses);
+console.log(appData);
+console.log('Ваш дневной бюджет: ' + appData.moneyDayBdget);
+console.log(typeof(appData.moneyDayBdget))
