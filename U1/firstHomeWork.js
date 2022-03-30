90e4ca0794b691f5460bbfa21be31a6a4b1737a7
Yakov Lemenkov <yakoff4@gmail.com>
2022-03-30 19:41:27 +0300

    home work is done

--- U1/firstHomeWork.js
+++ U1/firstHomeWork.js
@@ -10,18 +10,34 @@ let appData = {
     saving: false
 };
 
+appData.startBudget = appData.budget;
+
 for (let i = 0; i < 2; i++) {
     let exp = prompt('Введите обязательную статью расходов в этом месяце', ''),
         expValue = +prompt('Во сколько это обойдется', '');
 
-        if ((typeof(exp)) === 'string' && (typeof(exp)) != null && (typeof(expValue)) != null && exp != '' && expValue != '' && exp.length < 50) {
-            appData.expenses[exp] = expValue;
-            appData.budget -= expValue;
-        } else {
-            alert('Введите корректные значения');
-        }
-    }
+    if ((typeof (exp)) === 'string' && (typeof (exp)) != null && (typeof (expValue)) != null && exp != '' && expValue != '' && exp.length < 50) {
+        appData.expenses[exp] = expValue;
+        appData.budget -= expValue;
 
-alert('Ваш дневной бюджет: ' + (appData.budget / 30));
+    } else {
+        alert('Введите обязательные расходы');
+        i -= 1;
+    }
+}
+
+appData.moneyPerDay = appData.budget / 30;
+
+if (appData.moneyPerDay <= 10) {
+    console.log('Низкий доход!');
+} else if (appData.moneyPerDay >= 200) {
+    console.log('Высокий доход!');
+} else {
+    console.log('Средний доход!');
+}
+
+console.log('Изначальный бюджет: ' + appData.startBudget);
+alert('Ваш дневной бюджет: ' + appData.moneyPerDay);
+console.log('Ваш дневной бюджет: ' + appData.moneyPerDay);
+console.log('------------------------------------------------------------------------');
 console.log(appData);
-console.log('Ваш дневной бюджет: ' + (appData.budget / 30));
