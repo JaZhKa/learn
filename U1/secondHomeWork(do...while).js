d0dfa95b22e1e0d1d05ad418438527b195ddf946
Yakov Lemenkov <yakoff4@gmail.com>
2022-04-01 17:46:06 +0300

    second home work is done

---
+++ U1/secondHomeWork(do...while).js
@@ -0,0 +1,47 @@
+let money = +prompt('Ваш бюджет на месяц?', ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    saving: false
+};
+
+appData.startBudget = appData.budget;
+
+let n = 0;
+
+ do {
+    n++;
+    let exp = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        expValue = +prompt('Во сколько это обойдется', '');
+
+    if ((typeof (exp)) === 'string' && (typeof (exp)) != null && (typeof (expValue)) != null && exp != '' && expValue != '' && exp.length < 50) {
+        appData.expenses[exp] = expValue;
+        appData.budget -= expValue;
+
+    } else {
+        alert('Введите обязательные расходы');
+        n--;
+    }
+}
+while (n < 2);
+
+appData.moneyPerDay = appData.budget / 30;
+
+if (appData.moneyPerDay <= 10) {
+    console.log('Низкий доход!');
+} else if (appData.moneyPerDay >= 200) {
+    console.log('Высокий доход!');
+} else {
+    console.log('Средний доход!');
+}
+
+console.log('Изначальный бюджет: ' + appData.startBudget);
+alert('Ваш дневной бюджет: ' + appData.moneyPerDay);
+console.log('Ваш дневной бюджет: ' + appData.moneyPerDay);
+console.log('------------------------------------------------------------------------');
+console.log(appData);
