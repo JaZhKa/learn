b41de42f15b4040944b4b057d0be28d29f22a026
Yakov Lemenkov <yakoff4@gmail.com>
2022-06-14 20:49:33 +0300

    fooBar complete

--- fooBar.js
+++ fooBar.js
@@ -1,23 +1,19 @@
-/*
-My solution
-let arr = new Array(100);
+const arr = new Array(100);
 
-for(i=0; i<arr.length; i++) {
-    arr[i] = i;
+for (i = 0; i < arr.length; i++) {
+  arr[i] = i + 1;
 }
 
-// console.log(arr);
-
 arr.forEach((item) => {
-    if(item % 3 == 0 && item % 5 == 0) {
-        console.log(`${item}: foobar`);
-    } else if(item % 5 == 0){
-        console.log(`${item}: bar`);
-    } else if(item % 3 == 0){
-        console.log(`${item}: foo`);
-    }
+  if (item % 5 == 0 && item % 3 == 0) {
+    arr[item - 1] = 'fooBar';
+  } else if (item % 3 == 0) {
+    arr[item - 1] = 'foo';
+  } else if (item % 5 == 0) {
+    arr[item - 1] = 'bar';
+  }
 });
-*/
+console.log(arr);
 
 /*
 right solution
@@ -25,4 +21,16 @@ const arr = [...Array(100)].map((_, i) => i + 1);
 const res = arr.map(n => `${n % 3 ? '' : 'Foo'}${n % 5 ? '' : 'Bar'}` || n);
 
 console.log(res);
+*/
+
+/*
+add
+const a = +prompt('firs num', ''),
+      b = +prompt('second num', ''),
+      c = +prompt('third num', '')
+
+const arr = [...Array(100)].map((item, i) => ++i);
+const res = arr.map(n => `${n % a ? '' : 'foo'}${n % b ? '' : 'Bar'}${n % c ? '' : 'Plus'}` || n);
+
+alert(res);
 */
