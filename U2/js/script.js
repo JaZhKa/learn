33e559d10403739747f3fd316abbd8fb94ef9b63
Yakov Lemenkov <yakoff4@gmail.com>
2022-07-29 17:26:43 +0300

    add timer

--- U2/js/script.js
+++ U2/js/script.js
@@ -1,4 +1,7 @@
 window.addEventListener('DOMContentLoaded', function () {
+
+// Tabs
+
   'use strict';
   const tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
@@ -31,4 +34,45 @@ window.addEventListener('DOMContentLoaded', function () {
       }
     }
   });
+
+// Timer
+
+  const deadline = '2022-07-30 00:00:00';
+
+  function getTimeRamaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t / 1000) % 60),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        hours = Math.floor((t / (1000*60*60)));
+        // hours = Math.floor((t / 1000/60/60) % 24);   if need days
+        // days = Math.floor((t / (1000*60*60*24)));
+
+        return {
+          'total' : t,
+          'hours' : hours < 10 ? '0' + hours : hours,
+          'minutes' : minutes < 10 ? '0' + minutes : minutes,
+          'seconds' : seconds < 10 ? '0' + seconds : seconds
+        };
+      }
+      
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+    hours = timer.querySelector('.hours'),
+    minutes = timer.querySelector('.minutes'),
+    seconds = timer.querySelector('.seconds'),
+    timeInterval = setInterval(updeteClock, 1000);
+    
+    function updeteClock() {
+      let t = getTimeRamaining(endtime);
+      hours.textContent = t.hours;
+      minutes.textContent = t.minutes;
+      seconds.textContent = t.seconds;
+      
+      if(t.total < 1) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('timer', deadline);
 });
