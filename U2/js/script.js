4c6f393cb2297bba6ef70cb7e54de1f9f8a4c42d
Yakov Lemenkov <yakoff4@gmail.com>
2022-08-05 20:54:34 +0300

    modal

--- U2/js/script.js
+++ U2/js/script.js
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
 // Timer
 
-  const deadline = '2022-08-02 00:00:00';
+  const deadline = '2022-08-26 00:00:00';
 
   function getTimeRamaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -78,4 +78,22 @@ window.addEventListener('DOMContentLoaded', function () {
   }
 
   setClock('timer', deadline);
+
+// Modal
+
+  let more = document.querySelector('.more'),
+      overlay = document.querySelector('.overlay'),
+      close = document.querySelector('.popup-close');
+
+  more.addEventListener('click', function() {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+  });
+
+  close.addEventListener('click', function() {
+    overlay.style.display = 'none';
+    more.classList.remove('more-splash');
+    document.body.style.overflow = '';
+  });
 });
