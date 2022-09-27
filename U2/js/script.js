8db30df0fa353d9751646a8c3c042023e93e22af
Yakov Lemenkov <yakoff4@gmail.com>
2022-09-27 22:03:21 +0300

    add calc

--- U2/js/script.js
+++ U2/js/script.js
@@ -162,7 +162,7 @@ window.addEventListener('DOMContentLoaded', () => {
   sendForm(FORM);
   sendForm(MAIN_FORM);
 
-  //Slider
+  // Slider
 
   let slideIndex = 1;
   const SLIDES = document.querySelectorAll('.slider-item'),
@@ -192,7 +192,7 @@ window.addEventListener('DOMContentLoaded', () => {
     showSlides((slideIndex += n));
   }
   function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides((slideIndex = n));
   }
   PREV.addEventListener('click', () => plusSlides(-1));
   NEXT.addEventListener('click', () => plusSlides(1));
@@ -202,6 +202,53 @@ window.addEventListener('DOMContentLoaded', () => {
       if (e.target.classList.contains('dot') && e.target == DOTS[i - 1]) {
         currentSlide(i);
       }
+    }
+  });
+
+  // Calc
+
+  const PERSONS = document.querySelectorAll('.counter-block-input')[0],
+    REST_DAYS = document.querySelectorAll('.counter-block-input')[1],
+    PLACE = document.getElementById('select'),
+    TOTAL_VALUE = document.getElementById('total');
+  let personSum = 0,
+    daysSum = 0,
+    total = 0;
+
+  TOTAL_VALUE.innerHTML = 0;
+
+  PERSONS.addEventListener('change', function () {
+    personSum = +this.value;
+    total = (daysSum + personSum) * 400;
+
+    if (REST_DAYS.value === '') {
+      TOTAL_VALUE.innerHTML = 0;
+    } else if (REST_DAYS.value === '' || PERSONS.value === '') {
+      TOTAL_VALUE.innerHTML = 0;
+    } else {
+      TOTAL_VALUE.innerHTML = total;
+    }
+  });
+
+  REST_DAYS.addEventListener('change', function () {
+    daysSum = +this.value;
+    total = (daysSum + personSum) * 400;
+
+    if (PERSONS.value === '') {
+      TOTAL_VALUE.innerHTML = 0;
+    } else if (REST_DAYS.value === '' || PERSONS.value === '') {
+      TOTAL_VALUE.innerHTML = 0;
+    } else {
+      TOTAL_VALUE.innerHTML = total;
+    }
+  });
+
+  PLACE.addEventListener('change', function () {
+    if (REST_DAYS.value === '' || PERSONS.value === '') {
+      TOTAL_VALUE.innerHTML = 0;
+    } else {
+      let a = total;
+      TOTAL_VALUE.innerHTML = a * this.options[this.selectedIndex].value;
     }
   });
 });
